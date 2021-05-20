64805b6496185742c35d75e5d58e1d53f57e9f22
Artem <[programmerjunior0@gmail.com]>
2021-05-21 00:17:00 +0700

    lesson-1

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// 1 задание
+
+'use strict';
+let tc = 15;
+let tf = (9 / 5) * tc + 32;
+alert(tf);
+
+// 2 задание
+let name = 'Vasiliy'
+let admin = 'Vasiliy'
+alert(name);
+
+// 3* задание
+let task = 1000 + '108'; //ответ = 1000108
+alert(task);
